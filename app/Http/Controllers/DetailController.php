2b60a7ff7a86beb6be39661e3c2fc42c17c131b0
Willy kurniawan <willykurniawan50@gmail.com>
2020-08-06 16:56:01 +0700

    menambahkan halaman detail

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class DetailController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.home.home');
+        return view('pages.home.detail');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')->name('home');
 
+Route::get('/detail', 'DetailController@index')->name('detail');
 
 Route::prefix('admin')
     ->namespace('Admin')
